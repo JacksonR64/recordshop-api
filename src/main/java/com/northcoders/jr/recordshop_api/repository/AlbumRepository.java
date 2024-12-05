c19358af96bac52d7eae8aa99922fa076773227a
Jackson <jackson_rhoden@outlook.com>
2024-12-05 17:08:26 +0000

    feat(repository): change to extend Jpa

--- src/main/java/com/northcoders/jr/recordshop_api/repository/AlbumRepository.java
+++ src/main/java/com/northcoders/jr/recordshop_api/repository/AlbumRepository.java
@@ -1,9 +1,9 @@
 package com.northcoders.jr.recordshop_api.repository;
 
 import com.northcoders.jr.recordshop_api.model.Album;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlbumRepository extends CrudRepository<Album, Long > {
+public interface AlbumRepository extends JpaRepository<Album, Long > {
 }

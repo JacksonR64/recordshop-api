c39e6ed49726cb43562aeae2db76199255af4226
Jackson <jackson_rhoden@outlook.com>
2024-12-05 19:07:56 +0000

    fix(controller): add PostMapping annotation

--- src/main/java/com/northcoders/jr/recordshop_api/controller/AlbumController.java
+++ src/main/java/com/northcoders/jr/recordshop_api/controller/AlbumController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +23,7 @@ public class AlbumController {
         return new ResponseEntity<>(albums, HttpStatus.OK);
     }
 
+    @PostMapping()
     public ResponseEntity<Album> postAlbum(@RequestBody Album album) {
         Album newAlbum = albumService.postAlbum(album);
         HttpHeaders httpHeaders = new HttpHeaders();

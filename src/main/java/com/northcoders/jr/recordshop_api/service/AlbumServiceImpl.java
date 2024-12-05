11f7ff5b20386f0e4fa276096c77cb3677abe846
Jackson <jackson_rhoden@outlook.com>
2024-12-05 17:09:23 +0000

    fix(service): add Autowired instance and constructor

--- src/main/java/com/northcoders/jr/recordshop_api/service/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/jr/recordshop_api/service/AlbumServiceImpl.java
@@ -1,19 +1,33 @@
 package com.northcoders.jr.recordshop_api.service;
 
 import com.northcoders.jr.recordshop_api.model.Album;
+import com.northcoders.jr.recordshop_api.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class AlbumServiceImpl implements AlbumService{
+public class AlbumServiceImpl implements AlbumService {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    // Constructor for dependency injection
+    public AlbumServiceImpl(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+
     @Override
     public List<Album> getAllAlbums() {
-        return List.of();
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+        return albums;
     }
 
     @Override
     public Album postAlbum(Album album) {
-        return null;
+        return albumRepository.save(album);
     }
 }

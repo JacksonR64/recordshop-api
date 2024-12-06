d17e32a4e60571744937206ce7e45c7c89a794da
Jackson <jackson_rhoden@outlook.com>
2024-12-06 11:02:12 +0000

    fix(model): change annotation fixing lombok bug

--- src/main/java/com/northcoders/jr/recordshop_api/model/Album.java
+++ src/main/java/com/northcoders/jr/recordshop_api/model/Album.java
@@ -3,30 +3,35 @@ package com.northcoders.jr.recordshop_api.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor // Hibernate requires this
+@AllArgsConstructor // Full-args constructor
+@RequiredArgsConstructor // Required-args constructor for @NonNull fields
 public class Album {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    // TODO: FIX id defaulting to null instead of generating - check on postman first
+    @NonNull
     @Column(nullable = false)
     private String title;
 
+    @NonNull
     @Column(nullable = false)
     private String artist;
 
     @Enumerated(EnumType.STRING)
     @Column
-    private GENRE genre;
+    private GENRE genre = GENRE.UNKNOWN;
 
     @Column
     private int releaseYear = 0;
 
     @Column
     private int stockCount = 0;
-
 }

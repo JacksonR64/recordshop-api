1f71a18638956540ddcdd33034d47f848cdb57e1
Jackson <jackson_rhoden@outlook.com>
2024-12-16 10:34:06 +0000

    feat(service): add put and patch and delete abstract methods

--- src/main/java/com/northcoders/jr/recordshop_api/service/AlbumService.java
+++ src/main/java/com/northcoders/jr/recordshop_api/service/AlbumService.java
@@ -8,5 +8,8 @@ public interface AlbumService {
     List<Album> getAllAlbums();
     Album getAlbumById(Long id);
     Album postAlbum(Album album);
+    Album putAlbum(Long id, Album album);
+    Album patchAlbum(Long id, Album partialUpdate);
+    Album deleteAlbum(Long id);
 
 }
